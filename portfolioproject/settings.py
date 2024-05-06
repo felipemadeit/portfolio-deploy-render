6362e4c5e708d264c9e591508ec4710c07ebb09d
Julian Homez <felipemadeit@gmail.com>
2024-05-06 16:57:38 -0500

    attttempts

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i$)e@r@3z6ytm!2#!ipfxdh&6#7v=)-f-hz2_y@ye9kv-2fqs-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 

e485dabde6f8652a17979e5d25e4b6384f0640bf
Julian Homez <felipemadeit@gmail.com>
2024-05-06 16:55:58 -0500

    Attempts

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i$)e@r@3z6ytm!2#!ipfxdh&6#7v=)-f-hz2_y@ye9kv-2fqs-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
@@ -117,15 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
-STATIC_ROOT= 'static'
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media'
-
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
+STATIC_ROOT = BASE_DIR / '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

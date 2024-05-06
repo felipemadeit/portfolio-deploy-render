6e9fa7c93b3878bce097ae5da4f39c320688a410
Julian Homez <felipemadeit@gmail.com>
2024-05-06 00:29:35 -0500

    NO

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i$)e@r@3z6ytm!2#!ipfxdh&6#7v=)-f-hz2_y@ye9kv-2fqs-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
@@ -118,12 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT= 'static'
 
 MEDIA_URL = 'media/'
-MEDIAFILES_DIRS = os.path.join(BASE_DIR, 'media'),
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media'
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 

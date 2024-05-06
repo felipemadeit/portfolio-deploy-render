129dcac3b10dea98ead66c7a324fbe3bba585119
Julian Homez <felipemadeit@gmail.com>
2024-05-05 19:28:09 -0500

    Portfolio casi terminado

--- portfolioproject/urls.py
+++ portfolioproject/urls.py
@@ -21,4 +21,6 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
+    path('about', views.about_view, name='about'),
+    path('projects', views.project_view, name='projects'),
 ]

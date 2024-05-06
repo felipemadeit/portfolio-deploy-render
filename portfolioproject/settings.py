8078bfee52c411648a609d6593c4e9279e4db8d7
Julian Homez <felipemadeit@gmail.com>
2024-05-05 23:10:53 -0500

    AGG

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -127,6 +127,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
